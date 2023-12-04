6d6afc1eaf4c7b28e43986d3183217167b8f1bb8
Kaniltazo <angeldanielrm@icloud.com>
2023-12-04 00:32:14 -0500

    pruebas

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,9 +11,9 @@ document.getElementById("login-button").addEventListener("click", function () {
     
 
 
-    if (username === "anahy" && password === "123456789") {
+    if (username === "daniel@legalario.com" && password === "123456789") {
         // Las credenciales son correctas, redirige al usuario a la página de destino.
-        window.open ('/html/cotizador1.html');
+        window.open ('/html/selector.html');
         alert("Credenciales Correctas.");
     } else {
         alert("Credenciales incorrectas. Inténtalo de nuevo.");
